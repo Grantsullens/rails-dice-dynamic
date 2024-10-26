269d01a909360891ac1fbb49c846ecf3ecf45225
Grantsullens <sullens.ga@gmail.com>
2024-10-26 17:31:46 +0000

    Final

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,22 @@
+class MainController < ApplicationController
+  
+  def homepage
+    render({:template => "main/homepage"})
+  end
+
+  def flexible
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+    render({:template => "main/flexible"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  
+  get("/", {:controller => "main", :action => "homepage"}) 
 
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "main", :action => "flexible"}) 
 end
